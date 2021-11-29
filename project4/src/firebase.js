16908ef8ca1d40ba94f4e478c44bbcc146cc6eda
Danielle-91 <danielle.alamilla@gmail.com>
2021-11-29 14:33:27 -0500

    Firebase data displaying onscreen

--- project4/src/firebase.js
+++ project4/src/firebase.js
@@ -4,13 +4,19 @@ import 'firebase/database';
 
 // initializing Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCWrMFcwX3syag8EiXiL_ESaEoO44dU2u4",
-  authDomain: "project4-bootcamp.firebaseapp.com",
-  databaseURL: "https://project4-bootcamp-default-rtdb.firebaseio.com",
-  projectId: "project4-bootcamp",
-  storageBucket: "project4-bootcamp.appspot.com",
-  messagingSenderId: "662913192821",
-  appId: "1:662913192821:web:ffe393dbe2122a566c393c"
+  apiKey: "AIzaSyDTcslsmA02-64DP0_mz2laq9H13ZBqZlA",
+
+  authDomain: "bookshelf-app-8a7c4.firebaseapp.com",
+
+  databaseURL: "https://bookshelf-app-8a7c4-default-rtdb.firebaseio.com",
+
+  projectId: "bookshelf-app-8a7c4",
+
+  storageBucket: "bookshelf-app-8a7c4.appspot.com",
+
+  messagingSenderId: "308098864992",
+
+  appId: "1:308098864992:web:cf4437d960acca2337f005"
 };
 
 // const app = initializeApp(firebaseConfig);
