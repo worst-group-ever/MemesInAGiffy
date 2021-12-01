093e8e56e7aac7e4989b2a68ec581638095781cf
Danielle-91 <danielle.alamilla@gmail.com>
2021-12-01 17:17:32 -0500

    browse js and some styling done

--- project4/src/ErrorPage.js
+++ project4/src/ErrorPage.js
@@ -1,9 +1,17 @@
+<<<<<<< Updated upstream
 function ErrorPage(){
 
     return(
         <h1>Error Page Component</h1>
     )
 
+=======
+function ErrorPage() {
+
+    return (
+        <h1>error</h1>
+    )
+>>>>>>> Stashed changes
 }
 
 export default ErrorPage;
